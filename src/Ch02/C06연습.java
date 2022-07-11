20bccf714c9233ff2443555d4b742e528f24304a
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-07-11 16:41:03 +0900

    2022-07-11(Day4)

--- src/Ch02/C06연습.java
+++ src/Ch02/C06연습.java
@@ -3,6 +3,7 @@ package Ch02;
 public class C06연습 {
 
 	public static void main(String[] args) {
+//		4byte 음수를 지원하는 공간에 b라고 변수 부여, 동시에 100할당
 		int a;
 		a = 5;
 		
---
+++ src/Ch02/C07리터럴상수.java
@@ -0,0 +1,32 @@
+package Ch02;
+
+public class C07리터럴상수 {
+
+	public static void main(String[] args) {
+		
+		//상수 : 고정된 수
+		//상수 종류
+		//리터럴 상수 : 이름부여 X
+		//심볼릭 상수 : 이름부여 O (final 예약어)
+		
+		//정수 리터럴 기본자료형 : int
+		//실수 리터럴 기본자료형 : double
+		
+		//리터럴 접미사
+		//정수 : l, L(long)
+		//실수 : f, F(float)
+		
+		//예제1 
+//		byte num1 = (byte)129;
+//		long num2 = 123L;
+//		double num3 = 123.456;
+//		float num4 = 123.456F;
+//		
+//		System.out.println(num1);
+		
+		//예제2
+//		int num1 = 400000000;
+//		int num2 = 40000000L;
+	}
+
+}
